be2134b78b2c959fbdbdef29d7c46cbe2784ac7e
westerovs <westerovs@mail.ru>
2020-08-09 15:39:07 +0300

    отрендерил блоки

--- game.js
+++ game.js
@@ -10,29 +10,43 @@ const game = {
     ctx: null,
     ball: null,
     platform: null,
+    blocks: [],
+    rows: 4,
+    cols: 8,
     sprites: {
         background: null,
         ball: null,
-        platform: null
+        platform: null,
+        block: null,
     },
     init: function() {
         this.ctx = document.getElementById('mycanvas').getContext('2d');
     },
-    preload: function (callback) {
+    preload(callback) {
         let loaded = 0;
         let requered = Object.keys(this.sprites).length;
+
+        const onImageLoad = () => {
+            ++loaded;
+            if (loaded >= requered) {
+                callback();
+            }
+        };
 
         for (let key in this.sprites) {
             this.sprites[key] = new Image();
             this.sprites[key].src = `img/${key}.png`;
-
-            this.sprites[key].addEventListener('load', () => {
-                ++loaded;
-
-                if (loaded >= requered) {
-                    callback();
-                }
-            });
+            this.sprites[key].addEventListener('load', onImageLoad);
+        }
+    },
+    create() {
+        for (let row = 0; row < this.rows; row++) {
+            for (let col = 0; col < this.cols; col++) {
+                this.blocks.push({
+                    x: 64 * col + 65,
+                    y: 24 * row + 35
+                });
+            }
         }
     },
     run: function() {
@@ -50,14 +64,23 @@ const game = {
                 this.ball.x,
                 this.ball.y,
                 this.ball.width, // масштаб
-                this.ball.height, // масштаб
+                this.ball.height // масштаб
             );
             this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
+            this.renderBlock();
         });
+    },
+    renderBlock: function() {
+        for (const item of this.blocks) {
+            this.ctx.drawImage(this.sprites.block, item.x, item.y);
+        }
     },
     start: function() {
         this.init();
-        this.preload(() => this.run());
+        this.preload(() => {
+            this.create();
+            this.run();
+        });
     }
 };
 
