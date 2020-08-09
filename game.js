412947d9e38b14341e1a2b30d7995768a4090372
westerovs <westerovs@mail.ru>
2020-08-09 13:20:51 +0300

    сделал проверку на загрузку изобрежений

--- game.js
+++ game.js
@@ -1,15 +1,46 @@
 /* eslint-disable comma-dangle */
 /* eslint-disable no-multiple-empty-lines */
 
-
 const game = {
-    start: function() {
+    ctx: null,
+    sprites: {
+        background: null,
+        ball: null,
+        platform: null
+    },
+    init: function() {
         this.ctx = document.getElementById('mycanvas').getContext('2d');
-        const background = new Image();
-        background.src = 'img/background.png';
+    },
+    preload: function (callback) {
+        let loaded = 0;
+        let requered = Object.keys(this.sprites).length;
+
+        for (let key in this.sprites) {
+            this.sprites[key] = new Image();
+            this.sprites[key].src = `img/${key}.png`;
+
+            this.sprites[key].addEventListener('load', () => {
+                ++loaded;
+
+                if (loaded >= requered) {
+                    callback();
+                }
+            });
+        }
+    },
+    run: function() {
+        this.render();
+    },
+    render: function() {
         window.requestAnimationFrame(() => {
-            this.ctx.drawImage(background, 0, 0);
+            this.ctx.drawImage(this.sprites.background, 0, 0);
+            this.ctx.drawImage(this.sprites.ball, 0, 0);
+            this.ctx.drawImage(this.sprites.platform, 0, 0);
         });
+    },
+    start: function() {
+        this.init();
+        this.preload(() => this.run());
     }
 };
 
